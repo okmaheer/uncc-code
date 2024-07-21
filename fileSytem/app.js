b5d19cd5b251ed27bf6e08b857878bfd944a88e8
Maheer Akbar <maheerakbar@gmail.com>
2024-07-21 21:40:32 +0500

    buffer

--- fileSytem/app.js
+++ fileSytem/app.js
@@ -2,19 +2,22 @@ const fs = require('fs/promises');
 (async () => {
     try {
         const commandFileHandler = await fs.open("./command.txt", "r");
+        commandFileHandler.on("change",async () => {
+            const size = (await commandFileHandler.stat()).size;
+            const buff = Buffer.alloc(size);
+            const offset = 0;
+            const positiion = 0;
+            const length = buff.byteLength;
 
+             await commandFileHandler.read(buff, offset,length, positiion);
+            console.log(buff.toString('utf-8'))
+
+        })
         const watcher = fs.watch('./command.txt');
         for await (const event of watcher) {
             if (event.eventType === 'change') {
-                console.log('file is changed')
-                const size = (await commandFileHandler.stat()).size;
-                const buff = Buffer.alloc(size);
-                const offset = 0;
-                const positiion = 0;
-                const length = buff.byteLength;
-
-                const content = await commandFileHandler.read(buff, offset,length, positiion);
-                console.log(content)
+           commandFileHandler.emit('change')
+            
 
             }
         }
