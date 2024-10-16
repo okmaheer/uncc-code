f7a728204ee8ebf3510ae177d51dffd5174ad1ff
Maheer Akbar <maheerakbar@gmail.com>
2024-10-16 19:21:46 +0500

    change

--- networking/chat-app/client.js
+++ networking/chat-app/client.js
@@ -1,5 +1,17 @@
 const net =  require("net");
+const readLine = require('readline/promises');
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const socket = net.createConnection({host: "127.0.0.1", port:3008}, async () => {
+    console.log('connected to the server');
+    const message  = await rl.question("Enter your Message >");
+    socket.write(message);
 
-const socket = net.createConnection({host: "127.0.0.1", port:3008},()=>{
-console.log('connectedto the server');
-})
+});
+
+socket.on('data', (data)=>{
+    console.log("Message: ", data.toString('utf-8'));
+});
+
--- networking/chat-app/server.js
+++ networking/chat-app/server.js
@@ -2,8 +2,17 @@ const net = require('node:net');
 const port = 3008;
 const address = '127.0.0.1';
 const server = net.createServer();
+const clients = [];
 server.on('connection',(socket)=>{
     console.log('A new connection is made to the server');
+    socket.on('data', (data)=>{
+        console.log("Message from User: ", data.toString('utf-8'));
+        clients.map((s) => {
+            s.write(data);
+        });
+    });
+    
+    clients.push(socket);
 });
 server.listen(port,address,()=>{
     console.log("Server is running at",server.address());
